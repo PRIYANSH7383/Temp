c0d1d31b5f2517466248684f7ff4e8582e288886
PRIYANSH7383 <priyanshvagheshwari@gmail.com>
2025-01-12 13:46:07 +0530

    Added two c program for my first git excecise.
    Signed-off-by: PRIYANSH7383 <priyanshvagheshwari@gmail.com>

---
+++ priyansh.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main ()
+
+int num1,num2;
+// ask user to enter the two number	
+Printf ("please enter two different values\n");
+//read two numbre from user
+Scanf("%d,%d",&num1,&num2);
+If(num1>num2)
+
+{
+Printf("%d is largest\n",num 1);	
+
+}
+Else if (num2>num1)
+{
+	printf("%d is largest\n",num 2);
+}
+Else	
+	
+{
+
+Printf("both are equal\n");
+
+}
+Return 0;
+}
+	
+
+
